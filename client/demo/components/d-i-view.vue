c9796618dd4612736934bf44fe1c4b97a0c77305
zdliuccit <335758663@qq.com>
2018-02-06 17:53:51 +0800

    feat: 图片预览组件

--- client/demo/components/d-i-view.vue
+++ client/demo/components/d-i-view.vue
@@ -2,7 +2,18 @@
   <div class="dd-body">
     <d-head :back="true" title="IView"></d-head>
     <div class="dd-warp">
-      <ml-i-view></ml-i-view>
+      <img @click="imgShow=true" class="dd-ii-img" :src="url"/>
+      <ml-i-view v-model="imgShow" :url="url"></ml-i-view>
     </div>
   </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        url: 'https://img-np.xkeshi.cn/dev/crm/d3/59/2ab0f61a73309bc4ee16c09e40fe.jpeg',
+        imgShow: false,
+      }
+    }
+  }
+</script>
--- lib/ml/components/i-view/i-view.vue
+++ lib/ml/components/i-view/i-view.vue
@@ -1,19 +1,16 @@
 <template>
-  <div class="ml-i-view" :style="{ 'z-index':currentIndex|| 1001 }">
-    <span class="ii-test">{{zoom}}</span>
-    <div class="ml-i-img">
-      <img ref="mlIImg" :style="{transform:`translate3d(-50%, -50%, 0) scale(${zoom})`,
-      width:`${imgWidth}px`,
-      'margin-top':`${mTop}px`,
-      'margin-left':`${mLeft}px`}"
-           @load="imgLoad"
-           @error="imgError(imgObject)"
-           :src="imgObject.url"
-           :class="{'ii-scale':!isTouch}"
-           v-show="!loading"/>
-      <div v-show="loading">正在加载....</div>
+  <transition name="ii-view">
+    <div class="ml-i-view" :style="{ 'z-index':currentIndex|| 1001 }" v-show="value">
+      <div class="ml-i-img">
+        <img ref="mlIImg" :style="{transform:`translate3d(-50%, -50%, 0) scale(${zoom})`,
+      width:`${imgWidth}px`,'margin-top':`${mTop}px`,'margin-left':`${mLeft}px`}"
+             @load="imgLoad" @error="imgError" :src="url"
+             :class="{'ii-scale':!isTouch&&!this.animating}"
+             v-show="!loading"/>
+        <div v-show="loading">正在加载....</div>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 <script type="text/babel">
   import maskMixin from './../../mixins/mask-mixins'
@@ -22,27 +19,29 @@
   export default {
     name: 'ml-i-view',
     mixins: [maskMixin],
-    props: {},
+    props: {
+      value: Boolean,
+      url: String,
+    },
     data() {
       return {
-        imgObject: {
-          url: 'https://img-np.xkeshi.cn/dev/crm/d3/59/2ab0f61a73309bc4ee16c09e40fe.jpeg',
-          url2: 'https://img-np.xkeshi.cn/dev/crm/c9/e0/25556d4b08a8c40b7aeb125ca5a3.png',
-        },
         loading: false,
         dragObject: {},
-        starLine: 0,
-        zoom: 1,
+        starLine: 0, // 初始俩个点第1次距离
+        zoom: 1, // 放缩比
         compress: null, // 压缩比
         elWidth: null, // 外层宽度
         elHeight: null, // 外层高度
         imgWidth: null, // 当前图片宽度
         imgHeight: null, // 当前图片高度
-        mTop: 0,
-        mLeft: 0,
-        isTouch: false,
-        scrolling: false,
-        animating: false,
+        mTop: 0,  // margin-top 值
+        mLeft: 0, // margin-left 值
+        isTouch: false, // 是否touch
+        scrolling: false, // 是否放缩
+        animating: false, // 是否动画中
+        isTwoClick: false,  // 是否双击
+        startTime: null, // 第一次时间
+        timeFunc: null, // 定时器
       }
     },
     methods: {
@@ -59,35 +58,32 @@
       /**
        * 图片加载失败事件
        */
-      imgError(obj) {
+      imgError() {
         this.loading = false
-        obj.src = ''
       },
       /**
-       * 继续动画
+       * 继续执行一段距离滑行
        */
       continueTranslate(top, left) {
         this.animating = true
-        let ALPHA = 0.88
-        const yy = top - this.mTop
         const xx = left - this.mLeft
+        const yy = top - this.mTop
         const oldX = this.mLeft
         const oldY = this.mTop
         let diffX = 0
         let diffY = 0
+        let ALPHA = 0.88
         const animationLoop = () => {
-          ALPHA *= 0.98
+          ALPHA *= 0.95
           const resultX = Math.abs(diffX - xx) < 1
           const resultY = Math.abs(diffY - yy) < 1
           if (resultX && resultY) {
+            this.animating = false
             this.mLeft = left
             this.mTop = top
-            this.animating = false
           } else {
             diffX = diffX * ALPHA + (1 - ALPHA) * xx
             diffY = diffY * ALPHA + (1 - ALPHA) * yy
-            if (resultX) this.mLeft = left
-            if (resultY) this.mTop = top
             if (!resultX) this.mLeft = oldX + diffX
             if (!resultY) this.mTop = oldY + diffY
             animationFrame(animationLoop)
@@ -105,11 +101,9 @@
           const diffX = touch1.pageX - touch2.pageX
           const diffY = touch1.pageY - touch2.pageY
           this.starLine = Math.pow((diffX * diffX + diffY * diffY), 0.5)
-          const width = (this.zoom * this.imgWidth - this.elWidth) / 2
-          const height = (this.zoom * this.imgHeight - this.elHeight) / 2
-          this.dragObject.topThan = this.mTop / (height > 0 ? height : 0)
-          this.dragObject.leftThan = this.mLeft / (width > 0 ? width : 0)
         }
+        this.dragObject.topThan = this.mTop !== 0 ? this.mTop / this.reckonHeight(this.zoom) : 0
+        this.dragObject.leftThan = this.mLeft !== 0 ? this.mLeft / this.reckonWidth(this.zoom) : 0
         this.dragObject.startLeft = touch1.pageX
         this.dragObject.startTop = touch1.pageY
         this.dragObject.zoom = this.zoom
@@ -126,13 +120,11 @@
         dragObject.oldLeft = touch.pageX
         dragObject.oldTop = touch.pageY
         if (this.imgWidth * dragObject.zoom > this.elWidth) {
-          const cha = (this.imgWidth * dragObject.zoom - this.elWidth) / 2
-          if (Math.abs(this.mLeft) > cha) xx *= 0.3
+          if (Math.abs(this.mLeft) > this.reckonWidth(dragObject.zoom)) xx *= 0.3
           this.mLeft += xx
         }
         if (this.imgHeight * dragObject.zoom > this.elHeight) {
-          const cha = (this.imgHeight * dragObject.zoom - this.elHeight) / 2
-          if (Math.abs(this.mTop) > cha) yy *= 0.3
+          if (Math.abs(this.mTop) > this.reckonHeight(dragObject.zoom)) yy *= 0.3
           this.mTop += yy
         }
       },
@@ -142,20 +134,20 @@
        */
       touchEnd(dragDuration) {
         const dragObject = this.dragObject
-        const durationMile = Math.round(800 / dragDuration * 10)
-        let top = this.mTop
+        const durationMile = Math.round(1024 / dragDuration * 10)
         let left = this.mLeft
         if (this.imgWidth * dragObject.zoom > this.elWidth) {
           const xx = (dragObject.oldLeft - dragObject.startLeft) || 1
           left += durationMile * (xx / Math.abs(xx))
-          const cha = (this.imgWidth * dragObject.zoom - this.elWidth) / 2
+          const cha = this.reckonWidth(dragObject.zoom)
           if (left > cha) left = cha
           if (left < -cha) left = -cha
         }
+        let top = this.mTop
         if (this.imgHeight * dragObject.zoom > this.elHeight) {
           const yy = (dragObject.oldTop - dragObject.startTop) || 1
           top += durationMile * (yy / Math.abs(yy))
-          const cha = (this.imgHeight * dragObject.zoom - this.elHeight) / 2
+          const cha = this.reckonHeight(dragObject.zoom)
           if (top > cha) top = cha
           if (top < -cha) top = -cha
         }
@@ -172,50 +164,98 @@
         const diffX = touch1.pageX - touch2.pageX
         const diffY = touch1.pageY - touch2.pageY
         const line = Math.pow((diffX * diffX + diffY * diffY), 0.5) - this.starLine
-        let zoom = dragObject.zoom + (line / 2 / 75)
+        let zoom = Number(dragObject.zoom + (line / 2 / 75))
         /*eslint-disable*/
         if (zoom < 1) zoom = 1 - (1 - zoom ) * 0.1
         if (zoom > this.compress) zoom = this.compress + (zoom - 3) * 0.1
         this.zoom = zoom
-        const width = (zoom * this.imgWidth - this.elWidth ) / 2
-        const height = (zoom * this.imgHeight - this.elHeight ) / 2
-        this.mLeft = dragObject.leftThan * (width > 0 ? width : 0)
-        this.mTop = dragObject.leftThan * (height > 0 ? height : 0)
+        this.mLeft = dragObject.leftThan * this.reckonWidth(zoom)
+        this.mTop = dragObject.topThan * this.reckonHeight(zoom)
+      },
+      /**
+       * 返回 超出 宽度1/2
+       * @param {Number} zoom 当前比
+       * @param {Number} 值
+       */
+      reckonWidth(zoom) {
+        const width = (zoom * this.imgWidth - this.elWidth) / 2 || 0
+        return width > 0 ? width : 0
+      },
+      /**
+       * 返回 超出 高度1/2
+       * @param{Number} zoom 当前比
+       * @param {Number} 值
+       */
+      reckonHeight(zoom) {
+        const height = (zoom * this.imgHeight - this.elHeight) / 2 || 0
+        return height > 0 ? height : 0
       },
       /**
        * 初始化事件
        * @param {Object} $el 当前DOM
        */
       init($el) {
-        this.elWidth = $el.offsetWidth
-        this.elHeight = $el.offsetHeight
+        this.elWidth = document.documentElement.clientWidth
+        this.elHeight = document.documentElement.clientHeight
         $el.addEventListener('touchstart', (e) => {
           if (this.animating) return
-          this.animating = false
+          if (this.startTime) this.dragObject.duration = new Date() - this.startTime
+          if (!this.startTime) this.startTime = new Date()
+          if (this.dragObject.duration) this.startTime = null
           this.dragObject.startTime = new Date()
           this.touchStart(e)
           if (!this.isTouch) this.isTouch = e.touches.length > 1
         })
         $el.addEventListener('touchmove', (e) => {
-          e.preventDefault()
           if (this.animating) return
+          e.preventDefault()
           this.isTouch = true
           if (e.touches.length === 2) this.touchTwoMove(e)
           if (e.touches.length === 1) this.touchMove(e)
         })
         $el.addEventListener('touchend', () => {
           if (this.animating) return
-          const dragDuration = new Date() - this.dragObject.startTime // 间隔时长
-          if (!this.isTouch && dragDuration < 300) {
-            this.zoom = this.zoom > 1 ? 1 : 2
-          } else {
-            if (!this.scrolling) this.touchEnd(dragDuration)
+          const dragObject = this.dragObject
+          // 单次间隔时长
+          const duration = new Date() - this.dragObject.startTime
+          let zoom = this.zoom
+          if (this.isTouch) {
+            // 滑动事件
+            clearTimeout(this.timeFunc)
+            this.timeFunc = null
+            if (!this.scrolling) this.touchEnd(duration)
+            if (this.scrolling) {
+              if (zoom > this.compress) zoom = this.compress
+              if (zoom < 1) zoom = 1
+              if (dragObject.leftThan) this.mLeft = dragObject.leftThan * this.reckonWidth(zoom)
+              if (dragObject.topThan) this.mTop = dragObject.topThan * this.reckonHeight(zoom)
+            }
             this.isTouch = false
-            if (this.zoom > this.compress) this.zoom = this.compress
-            if (this.zoom < 1) this.zoom = 1
-            this.dragObject = {}
-            this.starLine = 0
+            this.zoom = zoom
             this.scrolling = false
+            this.starLine = 0
+            this.dragObject = {}
+          } else {
+            // 俩次点击时长<220双击
+            if (dragObject.duration && dragObject.duration < 220) {
+              // 双击事件
+              clearTimeout(this.timeFunc)
+              this.timeFunc = null
+              this.zoom = zoom > 1 ? 1 : 2
+              this.mLeft = this.mTop = 0
+              this.dragObject = {}
+            } else {
+              // 单击事件
+              if (this.timeFunc) return
+              this.timeFunc = setTimeout(() => {
+                this.timeFunc = null
+                this.dragObject = {}
+                this.startTime = null
+                this.zoom = 1
+                this.mLeft = this.mTop = 0
+                this.$emit('input', false)
+              }, 220)
+            }
           }
         })
       }
